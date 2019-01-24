5047d115706568f3724ff04d4739824e1889e022
bimalkeeth@gmail.com <Scala@1234>
2019-01-24 21:41:16 +1100

    Implement Business Objects

--- Moula/BIRuleProcessor/Implementations/AddressRuleProcessor.cs
+++ Moula/BIRuleProcessor/Implementations/AddressRuleProcessor.cs
@@ -1,9 +1,14 @@
+using System.Collections.Generic;
 using BIRuleProcessor.Interfaces;
+using CommonContracts;
 
 namespace BIRuleProcessor.Implementations
 {
     public class AddressRuleProcessor:IAddressRuleProcessor
     {
-        
+        public int CreateAddress(IEnumerable<AddressBo> addressBos)
+        {
+            
+        }
     }
 }
